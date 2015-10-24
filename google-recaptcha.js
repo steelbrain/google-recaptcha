1a3f151ec6dae08711e92e79107dc17d10b32f00
steelbrain <aneesiqbalbhatti@gmail.com>
2015-10-23 22:39:44 -0600

    :new: Rewrite using custom-element-base

--- google-recaptcha.js
+++ google-recaptcha.js
@@ -1,22 +1,33 @@
 'use strict'
 
-class GoogleRecaptcha extends HTMLElement {
-  createdCallback() {
-    this.style.display = 'block'
-    this.config = {
-      sitekey: this.getAttribute('sitekey') || '',
-      theme: this.getAttribute('theme') || 'light',
-      type: this.getAttribute('type') || 'image'
+const GoogleRecaptcha = require('custom-element-base')({
+  name: 'google-recaptcha',
+  config: {
+    sitekey: {
+      type: String,
+      default: ''
+    },
+    theme: {
+      type: String,
+      default: 'light'
+    },
+    type: {
+      type: String,
+      default: 'image'
     }
-  }
-  attachedCallback() {
-    if (!this.config.sitekey) {
+  },
+  created: function() {
+    this._captchaId = null
+    this.style.display = 'block'
+  },
+  initialize: function() {
+    if (!this.sitekey) {
       throw new Error('Google-Recaptcha: No sitekey specified')
     }
-    if (this.config.theme !== 'light' && this.config.theme !== 'dark') {
+    if (this.theme !== 'light' && this.theme !== 'dark') {
       throw new Error('Google-Recaptcha: Invalid theme specified')
     }
-    if (this.config.type !== 'image' && this.config.type !== 'audio') {
+    if (this.type !== 'image' && this.type !== 'audio') {
       throw new Error('Google-Recaptcha: Invalid type specified')
     }
     if (!GoogleRecaptcha._SCRIPT_LOADED) {
@@ -34,40 +45,31 @@ class GoogleRecaptcha extends HTMLElement {
         }
       }, 3000)
     }
-  }
-  attributeChangedCallback(attrName, _, attrValue) {
-    if (this.config.hasOwnProperty(attrName)) {
-      this.config[attrName] = attrValue
+  },
+  proto: {
+    reset: function() {
+      grecaptcha.reset(this._captchaId)
+    },
+    getResponse: function() {
+      return grecaptcha.getResponse(this._captchaId)
+    },
+    _render: function() {
+      this._captchaId = grecaptcha.render(this._container, this.config)
     }
   }
-  _render() {
-    this._captchaId = grecaptcha.render(this._container, this.config)
-  }
-  // Public method
-  reset() {
-    grecaptcha.reset(this._captchaId)
-  }
-  getResponse() {
-    return grecaptcha.getResponse(this._captchaId)
-  }
-
-  static loadScript() {
-    GoogleRecaptcha._SCRIPT_LOADED = true
-    const script = document.createElement('script')
-    script.setAttribute('async', '')
-    script.setAttribute('defer', '')
-    script.setAttribute('type', 'application/javascript')
-    script.setAttribute('src', GoogleRecaptcha._API_URL)
-    document.head.appendChild(script)
-  }
+})
+GoogleRecaptcha.loadScript = function() {
+  GoogleRecaptcha._SCRIPT_LOADED = true
+  const script = document.createElement('script')
+  script.setAttribute('async', 'async')
+  script.setAttribute('defer', 'defer')
+  script.setAttribute('type', 'application/javascript')
+  script.setAttribute('src', GoogleRecaptcha._API_URL)
+  document.head.appendChild(script)
 }
 GoogleRecaptcha._API_URL = 'https://www.google.com/recaptcha/api.js'
 GoogleRecaptcha._SCRIPT_LOADED = false
 
-GoogleRecaptcha.Element = document.registerElement('google-recaptcha', {
-  prototype: Object.create(GoogleRecaptcha.prototype)
-})
-
 if (typeof module !== 'undefined') {
-  module.exports = GoogleRecaptcha.Element
+  module.exports = GoogleRecaptcha
 }
